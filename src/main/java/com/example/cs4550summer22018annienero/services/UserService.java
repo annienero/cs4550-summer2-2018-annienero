1652202f204107e8480b9c04b4eb6c853c73abc6
Annie <annienero93@gmail.com>
2018-07-06 18:07:27 -0400

    user serivce method stubs

--- src/main/java/com/example/cs4550summer22018annienero/services/UserService.java
+++ src/main/java/com/example/cs4550summer22018annienero/services/UserService.java
@@ -5,25 +5,31 @@ import java.util.List;
 import com.example.cs4550summer22018annienero.models.User;
 import com.example.cs4550summer22018annienero.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserService {
-    //http://localhost:8080/register
 
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/api/register")
-    public User register(@RequestBody User user) {
+    @PostMapping("/api/user")
+    public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
 
     @GetMapping("/api/user")
     public List<User> findAllUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+    @GetMapping("/api/user/id")
+    public User findUserById() {
+        return new User();
+    }
+
+    @DeleteMapping("/api/user/id")
+    public void deleteUser() {
+        userRepository.delete(null);
     }
 }

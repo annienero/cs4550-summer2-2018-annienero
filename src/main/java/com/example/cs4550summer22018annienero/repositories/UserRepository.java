9a134f5fa445ff3ab356374e20e872d5d04081ec
Annie <annienero93@gmail.com>
2018-07-05 21:22:20 -0400

    create user service and repository

---
+++ src/main/java/com/example/cs4550summer22018annienero/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.cs4550summer22018annienero.repositories;
+
+
+import com.example.cs4550summer22018annienero.models.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
---
+++ src/main/java/com/example/cs4550summer22018annienero/services/UserService.java
@@ -0,0 +1,28 @@
+package com.example.cs4550summer22018annienero.services;
+
+import java.util.List;
+
+import com.example.cs4550summer22018annienero.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserService {
+    //http://localhost:8080/register
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/api/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/api/user")
+    public List<User> findAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+}

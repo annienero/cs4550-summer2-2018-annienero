431e854607ed4012d58af08cf807c244f5546ce9
Annie <annienero93@gmail.com>
2018-07-05 21:19:47 -0400

    create user class

---
+++ src/main/java/com/example/cs4550summer22018annienero/models/User.java
@@ -0,0 +1,8 @@
+package com.example.cs4550summer22018annienero.models;
+
+public class User {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+}

8133f55e2f60abba711397ab5e9f907f0166e9b4
Annie <annienero93@gmail.com>
2018-07-07 17:15:47 -0400

    reformat signup controller

--- src/main/webapp/jquery/components/register/register.controller.client.js
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -1,52 +1,33 @@
-// alert('Hello from register controller')
+(function () {
+    //equivalant to var $usernameFld = jQuery('username')
+    var $usernameFld = $('#username');  // # to get id
+    var $passwordFld = $('#password');
+    var $password2Fld = $('#password2');
 
-// var h1 = jQuery('h1')
-// h1.css('color', 'red')
+    var $registerBtn = jQuery('#registerBtn');
+    $registerBtn.click(registerHandler);
+    
+    function registerHandler() {
+        var usernameStr = $usernameFld.val();
+        var passwordStr = $passwordFld.val();
+        var password2Str = $password2Fld.val();
+    
+        // make JSON of user info
+        var userObj = {
+            username: usernameStr,
+            password: passwordStr,
+            password2: password2Str
+        }
+        
+        var userObjStr = JSON.stringify(userObj)
+        
+        fetch('/api/register', {
+            method: 'post',
+            body: userObjStr,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+})();
 
-//register button clicks
-
-var registerBtn = jQuery('#registerBtn') // # to get id
-registerBtn.click(function () {
-console.log('let goooo')
-})
-
-
-//grab input fields
-
-//var usernameFld = jQuery('username') vvv equivalant
-var usernameFld = $('#username')
-var passwordFld = $('#password')
-var password2Fld = $('#password2')
-
-
-function registerHandler() {
-var usernameStr = usernameFld.val()
-var passwordStr = passwordFld.val()
-var password2Str = password2Fld.val()
-
-console.log(usernameStr)
-console.log(passwordStr)
-
-// make JSON for user info
-var userObj = {
-username: usernameStr,
-password: passwordStr,
-password2: password2Str
-}
-
-var userObjStr = JSON.stringify(userObj)
-
-//console.log(userObj)
-
-fetch('/api/register', {
-method: 'post',
-body: userObjStr,
-headers: {
-'Content-Type': 'application/json'
-}
-})
-
-
-}
-
-registerBtn.click(registerHandler)

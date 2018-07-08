87be6ee521a346aa446f96af0edae9d3873501a2
Annie <annienero93@gmail.com>
2018-07-08 16:09:31 -0400

    register controller maybe more refined but mostly confused still

--- src/main/webapp/jquery/components/register/register.controller.client.js
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -3,13 +3,11 @@
     var $usernameFld, $passwordFld, $password2Fld;
     var $registerBtn;
 
-    // TODO: why do i need user service?
     var userService = new UserService();
     $(main);
 
     function main() {
-        //equivalant to var $usernameFld = jQuery('username')
-        $usernameFld = $('#username');  // # to get id
+        $usernameFld = $('#username');
         $passwordFld = $('#password');
         $password2Fld = $('#password2');
         $registerBtn = jQuery('#registerBtn');
@@ -31,8 +29,7 @@
         var userObjStr = JSON.stringify(userObj)
         
         userService.register(userObjStr, null) // TODO: how to get callback
-
-
+        
         // TODO what to do when username taken? just alert?
     }
 })();

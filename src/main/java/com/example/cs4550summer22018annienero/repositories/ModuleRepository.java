dcddcac072383d9b70d1366b5001bf76ae7e042f
Annie <annienero93@gmail.com>
2018-07-12 13:32:59 -0400

    create module repository

---
+++ src/main/java/com/example/cs4550summer22018annienero/repositories/ModuleRepository.java
@@ -0,0 +1,7 @@
+package com.example.cs4550summer22018annienero.repositories;
+
+import com.example.cs4550summer22018annienero.models.Module;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer> {
+}

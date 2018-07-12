1adffb3708c6de30e925342fd195127ad925789e
Annie <annienero93@gmail.com>
2018-07-12 13:32:52 -0400

    create lesson repository

---
+++ src/main/java/com/example/cs4550summer22018annienero/repositories/LessonRepository.java
@@ -0,0 +1,7 @@
+package com.example.cs4550summer22018annienero.repositories;
+
+import com.example.cs4550summer22018annienero.models.Lesson;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LessonRepository extends CrudRepository<Lesson, Integer> {
+}

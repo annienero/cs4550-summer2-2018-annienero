9a43510c68502b55547ca91c7220d023a10e7827
Annie <annienero93@gmail.com>
2018-07-23 12:39:43 -0400

    create WidgetRepository

---
+++ src/main/java/com/example/cs4550summer22018annienero/repositories/WidgetRepository.java
@@ -0,0 +1,8 @@
+package com.example.cs4550summer22018annienero.repositories;
+
+import com.example.cs4550summer22018annienero.models.Widget;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer> {
+
+}

75bd7d18d472cc88371210a3537024ffc097e4e9
Annie <annienero93@gmail.com>
2018-07-23 12:39:04 -0400

    create heading

---
+++ src/main/java/com/example/cs4550summer22018annienero/models/Heading.java
@@ -0,0 +1,13 @@
+package com.example.cs4550summer22018annienero.models;
+
+public class Heading extends Widget {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}

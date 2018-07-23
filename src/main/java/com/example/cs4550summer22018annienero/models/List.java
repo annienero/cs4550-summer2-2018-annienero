e0af33f9232e91848edb22862dd826d67558eb6d
Annie <annienero93@gmail.com>
2018-07-23 12:39:17 -0400

    create list

---
+++ src/main/java/com/example/cs4550summer22018annienero/models/List.java
@@ -0,0 +1,23 @@
+package com.example.cs4550summer22018annienero.models;
+
+public class List extends Widget {
+    private String listItems;
+    private ListType listType;
+
+    public String getListItems() {
+        return listItems;
+    }
+
+    public void setListItems(String listItems) {
+        this.listItems = listItems;
+    }
+
+    public ListType getListType() {
+        return listType;
+    }
+
+    public void setListType(ListType listType) {
+        this.listType = listType;
+    }
+
+}

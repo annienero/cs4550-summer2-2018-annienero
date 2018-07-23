440c979b6b193b34a8017f63d9fdf2f62a5a6e8a
Annie <annienero93@gmail.com>
2018-07-23 12:39:08 -0400

    create image

---
+++ src/main/java/com/example/cs4550summer22018annienero/models/Image.java
@@ -0,0 +1,13 @@
+package com.example.cs4550summer22018annienero.models;
+
+public class Image extends Widget {
+    private String src;
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+}
